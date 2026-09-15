1398acaf2628ee315eeca5706269f21ebf7893ca
ksanicc <sheldor414@gmail.com>
2026-09-16 01:35:52 +0300

    added socks5(local:10808) proxy via xray-core and added debug prints

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher
 from aiogram.filters import CommandStart
 from aiogram.types import Message
+from aiogram.client.session.aiohttp import AiohttpSession
 
 def platform_system():
     return platform.system()
@@ -12,17 +13,19 @@ def platform_system():
 load_dotenv()
 
 bot_token = os.getenv("BOT_TOKEN")
-
-bot = Bot(token=bot_token)
+session = AiohttpSession(proxy="socks5://127.0.0.1:10808")
+bot = Bot(token=bot_token, session=session)
 dp = Dispatcher()
 
 @dp.message(CommandStart())
 async def start_handler(message: Message):
+    print(f"Получена команда /start от {message.from_user.id}")
     user_name = message.from_user.full_name if message.from_user else "Пользователь"
     await message.answer(f"Your name is: {user_name}\n\nActive sys: {platform_system()}")
 
 async def main():
+    print("Запуск поллинга бота...")
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    asyncio.run(main()) 
