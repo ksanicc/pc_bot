8ecf4c0d406ee4d3bf6ca1e04721e95635c18a9f
ksanicc <sheldor414@gmail.com>
2026-09-16 00:49:15 +0300

    added /start handler and dotenv

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
 import platform
+import os
+import asyncio
+from dotenv import load_dotenv
+from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart
+from aiogram.types import Message
 
-print(f"Окружение поднято! Запуск на ОС: {platform.system()}")
+def platform_system():
+    return platform.system()
+
+load_dotenv()
+
+bot_token = os.getenv("BOT_TOKEN")
+
+bot = Bot(token=bot_token)
+dp = Dispatcher()
+
+@dp.message(CommandStart())
+async def start_handler(message: Message):
+    user_name = message.from_user.full_name if message.from_user else "Пользователь"
+    await message.answer(f"Your name is: {user_name}\n\nActive sys: {platform_system()}")
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
